d8ef6d87a8cec5241ddcf1055f5479f491a0bc1d
jonathan.trinidad <jonathan.trinidad@nube.sep.gob.mx>
2022-06-20 10:53:47 -0500

    Se agrega endpint de status del servicio

--- src/main/java/com/mx/animalia/application/AnimaliaApplication.java
+++ src/main/java/com/mx/animalia/application/AnimaliaApplication.java
@@ -1,10 +1,15 @@
 package com.mx.animalia.application;
 
+import com.mx.animalia.application.model.ContenedorResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-//@Configuration
+import javax.validation.Valid;
+
+@RestController
 @ComponentScan("com.mx.animalia")
 @SpringBootApplication
 public class AnimaliaApplication {
@@ -13,9 +18,13 @@ public class AnimaliaApplication {
 		SpringApplication.run(AnimaliaApplication.class, args);
 	}
 
-	//@Bean
-	//public JdbcTemplate jdbcTemplate(DataSource dataSource) {
-		//return new JdbcTemplate(dataSource);
-	//}
+	@GetMapping("/status")
+	public ContenedorResponse<?> altaCiudadanos(){
+		ContenedorResponse<?> contenedor = new ContenedorResponse<>();
+		contenedor.setCodigo(200L);
+		contenedor.setDescripcion("El servicio desplego de manera correcta.");
+		contenedor.setError(false);
+		return contenedor;
+	}
 
 }
