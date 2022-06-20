1a74a58c480b02b2ccc2279ef7c0098ad2b28333
jonathan.trinidad <jonathan.trinidad@nube.sep.gob.mx>
2022-06-20 18:29:40 -0500

    Se agrega la version del proyecto al consultar el path de status.

--- src/main/java/com/mx/animalia/application/AnimaliaApplication.java
+++ src/main/java/com/mx/animalia/application/AnimaliaApplication.java
@@ -1,6 +1,7 @@
 package com.mx.animalia.application;
 
 import com.mx.animalia.application.model.ContenedorResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -14,15 +15,17 @@ import javax.validation.Valid;
 @SpringBootApplication
 public class AnimaliaApplication {
 
+	@Value("${version.animalia}")
+	private String version;
 	public static void main(String[] args) {
 		SpringApplication.run(AnimaliaApplication.class, args);
 	}
 
-	@GetMapping("/status")
+	@GetMapping("${path.status}")
 	public ContenedorResponse<?> altaCiudadanos(){
 		ContenedorResponse<?> contenedor = new ContenedorResponse<>();
 		contenedor.setCodigo(200L);
-		contenedor.setDescripcion("El servicio desplego de manera correcta.");
+		contenedor.setDescripcion("El servicio desplego de manera correcta." + " Nombre del proyecto animalia." + " Version: "+ version);
 		contenedor.setError(false);
 		return contenedor;
 	}
